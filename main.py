a4066966b1d5eca8aa54c8d0b15bdb5c86c5e6dd
svicervlad <vlad.sadretdinov@jet.dev>
2022-06-18 15:16:00 +0000

    feat: init

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from loguru import logger
+
+logger.info("App lication stareted")
+
+
+app = FastAPI()
+
+
+@logger.catch
+@app.get("/")
+def read_root():
+    '''
+    Home page
+    '''
+    return {"Hello": "World"}
+
+@logger.catch
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    '''
+    Get some item
+    '''
+    return {"item_id": item_id, "q": q}
